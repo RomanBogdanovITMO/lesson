e70332a76649c7fc847ec5b3543ac06a8f545d5c
Roman <quke@inbox.ru>
2018-10-18 23:36:51 +0400

    first commit task05

---
+++ src/Task05/Task05.java
@@ -0,0 +1,18 @@
+package Task05;
+
+public class Task05 {
+    public static void main(String[] args) {
+        int value = 100;
+        int a = value % 10;
+        int b = (value / 10) % 10;
+        int c = (value / 100) % 10;
+
+        if(a > b & a > c){
+            System.out.println(a);
+        }else if (b > a & b > c){
+            System.out.println(b);
+        }else {
+            System.out.println(c);
+        }
+    }
+}
