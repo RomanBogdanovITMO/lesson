3fca3ee066552e318766bc8970da467f7b890b30
Roman <quke@inbox.ru>
2018-10-18 20:46:14 +0400

    second commit

--- src/task01/Task01.java
+++ src/task01/Task01.java
@@ -2,6 +2,16 @@ package task01;
 
 public class Task01 {
     public static void main(String[] args) {
-
+        int height,long1, width ;
+        height = 30;
+        long1 = 40;
+        width = 10;
+        int square = 2 *((long1 * width) + (width * height) + (long1 * height));
+        System.out.println("square = " + square);
+        if(height > width){
+            System.out.println("height = " + height);
+        }else {
+            System.out.println("width =" + width);
+        }
     }
 }
