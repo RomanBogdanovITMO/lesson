099dd7520e6d5c8c27ea8542f98f59ba090324d3
Roman <quke@inbox.ru>
2018-10-28 15:18:18 +0400

    изменения 28.10.2018(1)

--- src/lesson6/task02/Library.java
+++ src/lesson6/task02/Library.java
@@ -9,14 +9,13 @@ public class Library {
     Book[] books = new Book[size];
     int countadd =0;
     int countRemove =0;
-    int countBooks = 3;
+
 
     public void add(Book book){
         for(int i = 0; i< books.length; i++) {
             if (books[i] == null) {
                 books[i] = book;
                 countadd++;
-                System.out.println("количество книг на полке: " + countadd);
                 return;
             }
 
@@ -27,21 +26,24 @@ public class Library {
     public void remove(Book book){
         for(int i = 0; i < books.length; i++) {
             if (book.getName().equals(books[i].getName())){
-                book.getName().equals(null);
+                books[i]= null;
                 countRemove++;
-                System.out.println("мест свободно: " + countRemove);
                 return;
             }
 
         }
         System.out.println("полка свободна");
     }
-   /* public void showLibrary(){
 
-        System.out.println("количество свободных мест на полке: " + count );
-        System.out.println("количество книг на полке: " + countBooks);
 
-    }*/
+     public void showLibrary(){
+
+        System.out.println("количество книг на полке: " + countadd );
+        System.out.println(Arrays.toString(books));
+        System.out.println("мест свободно: " + countRemove );
+
+
+    }
 }
 
 
