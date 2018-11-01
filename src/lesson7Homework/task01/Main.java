5e9263c2a950d759730f3f2e5fedb0500a28f727
Roman <quke@inbox.ru>
2018-11-01 23:31:55 +0400

    homeWork 30.10.2018

--- src/lesson7Homework/task01/Main.java
+++ src/lesson7Homework/task01/Main.java
@@ -3,6 +3,9 @@ package lesson7Homework.task01;
 public class Main {
     public static void main(String[] args) {
         Pupil pupil = new Pupil(1);
-
+        String[] ex = {"География","Математика"};
+        int[] mark = {3,2};
+        pupil.setExam(ex,mark);
+        System.out.println(pupil.toString());
     }
 }
--- src/lesson7Homework/task01/Pupil.java
+++ src/lesson7Homework/task01/Pupil.java
@@ -1,16 +1,19 @@
 package lesson7Homework.task01;
 
+import java.util.Arrays;
+
 public class Pupil {
     private int id;
-    private ExamResult [] examResults;
+    private ExamResult[] examResults;
+    final static int COUNT_MAX = 2;
 
     public Pupil(int id) {
         this.id = id;
+        this.examResults = new ExamResult[COUNT_MAX];
     }
 
 
-
-    private class ExamResult{
+    private class ExamResult {
         String name;
         int mark;
         boolean passed;
@@ -18,6 +21,7 @@ public class Pupil {
         public ExamResult(String name, int mark) {
             this.name = name;
             this.mark = mark;
+            this.passed = mark > 2;
         }
 
         public String getName() {
@@ -36,12 +40,39 @@ public class Pupil {
             this.mark = mark;
         }
 
-        public boolean isPassed() {
+        public boolean getPassed() {
             return passed;
         }
 
-        public void setPassed(boolean passed) {
-            this.passed = passed;
+        @Override
+        public String toString() {
+            if(this.passed == true){
+                return name + " сдал";
+            }
+            return name + " не сдал";
         }
     }
-}
+
+        public void setExam(String[] lesson, int[] marks) {
+            if (lesson.length != marks.length) {
+                System.out.println("ошибочные данные");
+                return;
+            }
+            for (int i = 0; i < lesson.length; i++) {
+                this.examResults[i] = new ExamResult(lesson[i], marks[i]);
+            }
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder examsString = new StringBuilder();
+            for (int i = 0; i < examResults.length; i++) {
+                examsString.append("\n");
+                examsString.append(examResults[i].toString());
+            }
+
+            return "Студент: " + this.id
+                    + examsString.toString();
+        }
+    }
+
