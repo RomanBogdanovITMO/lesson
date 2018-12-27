101fa5440c4b7e511a8e7c3c50fae96833984e10
Roman <quke@inbox.ru>
2018-12-27 23:52:07 +0400

    homeWork 27.12.2018

--- src/lesson21Homework/Task01/Reader.java
+++ src/lesson21Homework/Task01/Reader.java
@@ -12,9 +12,9 @@ public class Reader implements Runnable {
     Library library;
     Book book;
 
-    public Reader(String name, Library library, Book book) {
+    public Reader(String name, Library library ) {
         this.name = name;
-        this.book = book;
+       // this.book = book;
         this.library = library;
         listBooks = new ArrayList<>();
     }
@@ -34,20 +34,53 @@ public class Reader implements Runnable {
     @Override
     public void run() {
         Scanner scanner = new Scanner(System.in);
-       String str = scanner.nextLine();
-       if(str.equals("забрать")) {
-           listBooks.add(book);
-           library.remove(library.getMapBooks(), book);
-           System.out.println(library.getMapBooks());
-       }else if(str.equals("вернуть")) {
-           int count = listBooks.size();
-           if (count == 0){
-               System.out.println("у вас нет книг что бы вернуть");
-           }
-           library.addBooks(book);
-           System.out.println(library.getMapBooks());
-       }else {
-           System.out.println("указана неверная комманда");
-       }
+        String vibor = null;
+        boolean flag = true;
+        System.out.println("список книг на данный момент: " + library.getMapBooks());
+        while (flag) {
+            System.out.println("Какую книгу вы выбираете: ");
+            vibor = scanner.nextLine();
+            System.out.println(vibor);
+            for (Book bk : library.getMapBooks().keySet()) {
+                if ((bk.getNameBook()).equals(vibor)) {
+                    listBooks.add(bk);
+                    System.out.println("Добавили книгу");
+                    library.remove(library.getMapBooks(), bk);
+                    System.out.println(listBooks);
+                    System.out.println("Вы нужна еще книга: да, нет");
+                    String str = scanner.nextLine();
+                    if (str.equals("нет")) {
+                        flag = false;
+                    }
+                    break;
+                }
+            }
+        }
+        System.out.println("");
+        System.out.println("доступные книги в библиотеке: " + library.getMapBooks() + "\n");
+        boolean flag1 = true;
+        while (flag1) {
+            System.out.println("какую книгу хотите вернуть?");
+            String str2 = scanner.nextLine();
+            for (Book bk : listBooks) {
+                if (str2.equals(bk.getNameBook())) {
+                    System.out.println(bk.getNameBook());
+                    listBooks.remove(bk);
+                    library.addBooks(bk);
+                    System.out.println("Книги в библиотеке: " + library.getMapBooks());
+                    System.out.println("Повторить операцию: да, нет ?");
+                    String str3 = scanner.nextLine();
+                    if (str3.equals("нет")) {
+                        flag1 = false;
+                    }
+                    break;
+                }
+
+            }
+        }
+        System.out.println("______________________________________________");
+        System.out.println("Всего книг в библиотеке: " + library.getMapBooks());
+        System.out.println("_______________________________________________");
+        System.out.println("Остаток книг у читателя" + listBooks);
     }
 }
